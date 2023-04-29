1d086f1cf7428d98a95fd7de46cb430e6ec0eac2
Mingjie Shen <shen497@purdue.edu>
2023-04-29 18:27:21 +0000

    Update

--- gen-wllvm.sh
+++ gen-wllvm.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-export PATH=`realpath ../gcc-arm-none-eabi-8-2018-q4-major/bin`:$PATH
 make clean
-#make -j OptLIB=2 OptSRC=2 all tshow
-genbc ../bitcode-db/Avem -c "make -j OptLIB=2 OptSRC=2 all tshow"
+
+TOOLCHAIN_BIN=$(realpath $RTOSExploration/toolchain/gcc-arm-none-eabi-8-2018-q4-major/bin)
+export PATH=$TOOLCHAIN_BIN:$PATH
+export LLVM_COMPILER=hybrid
+export LLVM_COMPILER_PATH=/usr/lib/llvm-14/bin
+export BINUTILS_TARGET_PREFIX=$TOOLCHAIN_BIN/arm-none-eabi
+make -j$(nproc) OptLIB=2 OptSRC=2 all tshow CC=wllvm
+extract-bc main.elf
